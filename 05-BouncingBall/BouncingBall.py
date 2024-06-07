cee38483d2172c2b017dc22189ddad534188ed5d
nickj <nickjl06@icloud.com>
2024-06-07 10:26:25 -0500

    finished

--- 05-BouncingBall/BouncingBall.py
+++ 05-BouncingBall/BouncingBall.py
@@ -2,11 +2,31 @@ import pygame
 import sys
 import random
 
+balls = []
 
 # You will implement this module ENTIRELY ON YOUR OWN!
 
-# TODO: Create a Ball class.
-# TODO: Possible member variables: screen, color, x, y, radius, speed_x, speed_y
+# TODO: Create a Ball class.\
+class Ball:
+    def __init__(self, screen, x, y, x_speed, y_speed, ball_size, color):
+        self.radius = ball_size
+        self.x_speed = x_speed
+        self.y_speed = y_speed
+        self.screen = screen
+        self.ball_color = color
+        self.x = x
+        self.y = y
+    def move(self):
+        self.y = self.y + self.y_speed
+        self.x = self.x + self.x_speed
+        if self.x >= self.screen.get_width() or self.x <= 0:
+            self.x_speed = -self.x_speed
+        if self.y >= self.screen.get_height() or self.y <= 0:
+            self.y_speed = -self.y_speed
+# dTODO: Possible member variables: screen, color, x, y, radius, speed_x, speed_y
+    def draw(self):
+        pygame.draw.circle(self.screen, self.ball_color, (self.x, self.y), radius = self.radius)
+
 # TODO: Methods: __init__, draw, move
 
 
@@ -17,8 +37,19 @@ def main():
     screen.fill(pygame.Color('gray'))
     clock = pygame.time.Clock()
 
+
     # TODO: Create an instance of the Ball class called ball1
 
+    balls = []
+    for ball_number in range(0,20):
+        ball_x_speed = random.randint(1, 20)
+        ball_y_speed = random.randint(1, 20)
+        ball_size = random.randint(1, 10)
+        ball_color1 = random.randint (1,255)
+        ball_color2 = random.randint(1, 255)
+        ball_color3 = random.randint(1, 255)
+        b = Ball(screen, 50, 51, ball_x_speed, ball_y_speed, ball_size,(ball_color1, ball_color2, ball_color3))
+        balls.append(b)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -26,10 +57,12 @@ def main():
 
         clock.tick(60)
         screen.fill(pygame.Color('gray'))
+        # dTODO: Move the ball
 
-        # TODO: Move the ball
-        # TODO: Draw the ball
-
+        # dTODO: Draw the ball
+        for ball in balls:
+            ball.move()
+            ball.draw()
         pygame.display.update()
 
 
